47ce36ef9b8343508d253b19094f59943c0eb80d
Patrice Wietmaier <account@pwietmaier.de>
2023-12-18 00:40:24 +0100

    Add basic auth restriction

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,23 @@
 import type { Handle, HandleServerError } from '@sveltejs/kit';
 import { auth } from '$lib/server/ts/lucia';
+import { ADMIN_LOGIN } from '$env/static/private';
+import { dev } from '$app/environment';
 
 export const handle: Handle = async ({ event, resolve }) => {
+  if (!dev) {
+    // BASIC AUTH (Production Only)
+    const basicAuthHeader = event.request.headers.get('Authorization');
+
+    if (basicAuthHeader !== `Basic ${btoa(ADMIN_LOGIN)}`) {
+      return new Response('Not authorized. SpeedCubeShare is currently under construction...', {
+        status: 401,
+        headers: {
+          'WWW-Authenticate': 'Basic realm="Access to SpeedCubeShare", charset="UTF-8"',
+        },
+      });
+    }
+  }
+
   event.locals.auth = auth.handleRequest(event);
   return resolve(event);
 };
